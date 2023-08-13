9611fb9b7d3d2711466dfec9889ab5d5e00c4404
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2023-08-13 08:39:32 -0600

    update

---
+++ data/processhistory.sh
@@ -0,0 +1,4 @@
+jar -cvfM  EX-002_Public_Voting_History_List_Part1.txt.zip EX-002_Public_Voting_History_List_Part1.txt
+jar -cvfM  EX-002_Public_Voting_History_List_Part2.txt.zip EX-002_Public_Voting_History_List_Part2.txt
+jar -cvfM  EX-002_Public_Voting_History_List_Part3.txt.zip EX-002_Public_Voting_History_List_Part3.txt
+jar -cvfM  EX-002_Public_Voting_History_List_Part4.txt.zip EX-002_Public_Voting_History_List_Part4.txt
