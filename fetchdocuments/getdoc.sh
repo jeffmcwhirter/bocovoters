6fff9880bdf614e862d7bf8184268934e6a267d9
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2025-08-05 07:01:51 -0600

    snapshot

---
+++ fetchdocuments/getdoc.sh
@@ -0,0 +1,15 @@
+curl --output "${2}"  "https://documents.bouldercolorado.gov/WebLink/edoc/${1}/CAC%20Agenda%20-%2001.03.2023.pdf?dbid=0^&repo=LF8PROD2" \
+  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:139.0) Gecko/20100101 Firefox/139.0' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
+  -H 'Accept-Language: en-US,en;q=0.5' \
+  -H 'Accept-Encoding: gzip, deflate, br, zstd' \
+  -H 'DNT: 1' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: WebLinkSession=ztjbtm3qn4zi5awh0zcajvzw; lastSessionAccess=638869451474080245; AcceptsCookies=1; MachineTag=06939ccd-d62f-49c2-a3d4-8b2c66400d9a' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Priority: u=0, i' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache'
